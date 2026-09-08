5350c483727f4fa1597fd963dea1d39940d084de
sherifbai <ruzmetov_sh@tuneconsulting.net>
2026-09-08 13:56:18 +0500

    name the delete conversations helpers like the other test helpers

--- utils/helpers/index.ts
+++ utils/helpers/index.ts
@@ -1,4 +1,4 @@
-export * from './delete-conversations-helpers';
+export * from './delete-conversations-test-helpers';
 export * from './domain-test-helpers';
 export * from './generated-services';
 export * from './locale';
